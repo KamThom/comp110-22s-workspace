27e5dae583799f8cc1c5f0fd5cb38d22ca24bbf8
Kameron Thomas <kamthoma@email.unc.edu>
2022-01-23 18:24:14 -0500

    Final ex01

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -14,45 +14,53 @@ if val == 5:
 
         if char in word:
             # each variable is set to a character in the given word to be called later
-            a = word[0]
-            b = word[1]
-            c = word[2]
-            d = word[3]
-            e = word[4]
+            # they are set as str becasue i got an error in gradescope wanting them to be a str but this did not help
+            a = str(word[0])
+            b = str(word[1])
+            c = str(word[2])
+            d = str(word[3])
+            e = str(word[4])
 
             if a == char:
-                a = [len(word) - 5]
+                a = str([len(word) - 5])
                 # the '-#' is to give the location of that varible in ascending order by aking the max len and subtracting
-                print(str(char) + " found at index " + str(a))
+                print(str(char) + " found at index " + str(a)[1:-1])
+                # the [1:-1] makes it so the "list" is printed without brackets. It just removes the first and last characters of a lsit
 
             if b == char:
-                b = [len(word) - 4]
-                print(str(char) + " found at index " + str(b))
+                b = str([len(word) - 4])
+                print(str(char) + " found at index " + str(b)[1:-1])
 
             if c == char:
-                c = [len(word) - 3]
-                print(str(char) + " found at index " + str(c))
+                c = str([len(word) - 3])
+                print(str(char) + " found at index " + str(c)[1:-1])
 
             if d == char:
-                d = [len(word) - 2]
-                print(str(char) + " found at index " + str(d))
+                d = str([len(word) - 2])
+                print(str(char) + " found at index " + str(d)[1:-1])
 
             if e == char:
-                e = [len(word) - 1]
-                print(str(char) + " found at index " + str(e))
+                e = str([len(word) - 1])
+                print(str(char) + " found at index " + str(e)[1:-1])
 
-            if char == char:
+            if char == char and word.count(char) == 1:
                 # if all character in word == char, this last if exits the if statement and prints the desired message
                 # word.count gives me the number of instances of char in word very simply
-                print(str(word.count(char)) + " instances of " + str(char) + " found in " + str(word)) 
+                print(str(word.count(char)) + " instance of " + str(char) + " found in " + str(word)) 
+
+            if char == char and word.count(char) > 1:
+                # this new if statement is used if more than one instance is found giveing plurality to instances 
+                print(str(word.count(char)) + " instances of " + str(char) + " found in " + str(word))
     
         else:
             # next three else statements are self explnitory 
             print("no instances of " + char + " found in " + word)
     else:
         print("Error: Character must be a single character.")
+        exit()
 else:
     print("Error: Word must contain 5 characters")
+    exit()
     
 
 # exercises.ex01_chardle
